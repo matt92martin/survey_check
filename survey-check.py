e2104986bd96bd7103c61129f86fdda343aa2ba6
mmartin <matt92martin@gmail.com>
2019-12-11 11:09:11 -0800

    Changes to structure and path behavior

--- survey-check.py
+++ survey-check.py
@@ -1,13 +1,42 @@
 from sys import exit
 import os
+import re
 import argparse
+from lxml import etree as ET
 
-from survey import Survey
+class Survey:
+    def __init__(self, path):
+        self.tree = self.open(path)
+        self.rels = set()
+        self.images()
+        self.others()
+
+    def open(self, path):
+        return ET.parse(path, ET.XMLParser(recover=True))
+
+    def images(self):
+        for img in self.tree.findall('.//img'):
+            if '[rel ' in img.attrib['src']:
+                self.rels.add(self.clean_rels(img.attrib['src']))
+
+    def others(self):
+        for style in self.tree.findall('.//style') + self.tree.findall('.//res'):
+            resources = re.findall(r'\s*\[rel\s+\w+\.\w+\]\s*', ET.tostring(style))
+
+            for res in resources:
+                self.rels.add(self.clean_rels(res))
+
+    def clean_rels(self, rel):
+        match = re.search(r'\s*\[rel\s+(\w+\.\w+)\s*\]\s*', rel)
+        return match.group(1)
+
+    def resources(self):
+        return self.rels
 
 
 class SurveyCheck:
     def __init__(self, opts):
-        self.survey_path = os.path.join(opts.survey, 'survey.xml')
+        self.survey_path = os.path.join(os.path.abspath(opts.survey), 'survey.xml')
         self.path = self.set_path(self.survey_path)
         self.static = os.path.join(self.path, 'static')
         self.survey = Survey(self.survey_path)
--- survey/__init__.py
+++
@@ -1,1 +0,0 @@
-from Survey import Survey
